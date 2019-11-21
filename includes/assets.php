fd966060d039e285a1de655f73c981e7ba9a07b6
Alexandru Muscalu <alexandru@crowdfavorite.com>
2019-11-21 16:45:02 +0200

    store constant in var and reuse it for including minified versions

--- includes/assets.php
+++ includes/assets.php
@@ -48,30 +48,32 @@ function style_admin_dashboard($classes)
  */
 function load_admin_assets()
 {
+	$env = (defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) ? '.' : '.min.';
+
 	// Register the custom plugin scripts.
 	// This is the compiled js file.
 	wp_register_script(
 		OSDXP_DASHBOARD_HANDLE,
-		OSDXP_DASHBOARD_URL . ((defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) ? 'build/app.js' : 'build/app.min.js'),
+		OSDXP_DASHBOARD_URL . 'build/app' . $env . 'js',
 		[ 'jquery' ],
-		filemtime(OSDXP_DASHBOARD_DIR . ((defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) ? 'build/app.js' : 'build/app.min.js')),
+		filemtime(OSDXP_DASHBOARD_DIR . 'build/app' . $env . 'js'),
 		false
 	);
 
 	// Register the OSDXP style.
 	wp_register_style(
 		OSDXP_DASHBOARD_HANDLE,
-		OSDXP_DASHBOARD_URL . ((defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) ? 'build/style-admin.css' : 'build/style-admin.min.css'),
+		OSDXP_DASHBOARD_URL . 'build/style-admin' . $env . 'css',
 		[],
-		filemtime(OSDXP_DASHBOARD_DIR . ((defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) ? 'build/style-admin.css' : 'build/style-admin.min.css')),
+		filemtime(OSDXP_DASHBOARD_DIR . 'build/style-admin' . $env . 'css'),
 		false
 	);
 
 	wp_register_style(
 		OSDXP_DASHBOARD_HANDLE . 'wp-admin',
-		OSDXP_DASHBOARD_URL . ((defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) ? 'build/style-wp-admin.css' : 'build/style-wp-admin.min.css'),
+		OSDXP_DASHBOARD_URL . 'build/style-wp-admin' . $env . 'css',
 		[],
-		filemtime(OSDXP_DASHBOARD_DIR . ((defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) ? 'build/style-wp-admin.css' : 'build/style-wp-admin.min.css')),
+		filemtime(OSDXP_DASHBOARD_DIR . 'build/style-wp-admin' . $env . 'css'),
 		false
 	);
 
@@ -108,21 +110,23 @@ function load_admin_assets()
  */
 function load_frontend_assets()
 {
+	$env = (defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) ? '.' : '.min.';
+
 	// Register the plugin admin bar custom style.
 	wp_register_style(
 		OSDXP_DASHBOARD_HANDLE . '-admin-bar',
-		OSDXP_DASHBOARD_URL . ((defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) ? 'build/admin-bar.css' : 'build/admin-bar.min.css'),
+		OSDXP_DASHBOARD_URL . 'build/admin-bar' . $env . 'css',
 		[ 'admin-bar' ],
-		filemtime(OSDXP_DASHBOARD_DIR . ((defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) ? 'build/admin-bar.css' : 'build/admin-bar.min.css')),
+		filemtime(OSDXP_DASHBOARD_DIR . 'build/admin-bar' . $env . 'css'),
 		false
 	);
 
 	// Register the plugin wp admin bar custom style.
 	wp_register_style(
 		OSDXP_DASHBOARD_HANDLE . '-wp-admin-bar',
-		OSDXP_DASHBOARD_URL . ((defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) ? 'build/wp-admin-bar.css' : 'build/wp-admin-bar.min.css'),
+		OSDXP_DASHBOARD_URL . 'build/wp-admin-bar' . $env . 'css',
 		[ 'admin-bar' ],
-		filemtime(OSDXP_DASHBOARD_DIR . ((defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) ? 'build/wp-admin-bar.css' : 'build/wp-admin-bar.min.css'))
+		filemtime(OSDXP_DASHBOARD_DIR . 'build/wp-admin-bar' . $env . 'css')
 	);
 
 	if (is_dxp_dashboard_active_for_current_user()) {

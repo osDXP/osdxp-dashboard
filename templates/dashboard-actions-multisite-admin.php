8a76b2706ba17b3fb17de21ca2ca71d033ac4590
Alexandru Muscalu <alexandru@crowdfavorite.com>
2019-11-25 14:38:11 +0200

    add create functionality hooks for 3rd party plugins/partners to register new OSDXP dashboard widgets

--- templates/dashboard-actions-multisite-admin.php
+++ templates/dashboard-actions-multisite-admin.php
@@ -102,24 +102,24 @@
 		<?php endif; ?>
 
 		<?php
-        if (is_plugin_active('multilingualpress/multilingualpress.php')) {
-            $language_manager = (array)get_network_option(0, 'multilingualpress_modules', []);
-            if (! empty($language_manager) && ! empty($language_manager['language-manager'])) {
-                ?>
-                    <a href="/wp-admin/network/admin.php?page=language-manager" class="col">
-                        <div class="postbox">
-                            <div>
-                                <div class="group">
-                                    <div class="dashicons-before dashicons-admin-site-alt3"></div>
-                                    <span><?php esc_html_e('Settings', 'osdxp-dashboard'); ?></span>
-                                    <p><?php esc_html_e('Add New Language', 'osdxp-dashboard'); ?></p>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-					<?php
-            }
-        }
+		$custom_create_functionality_modules = [];
+		$custom_create_functionality_modules = apply_filters('osdxp_dashboard_create_functionality', $custom_create_functionality_modules);
+
+		foreach ($custom_create_functionality_modules as $custom_module) {
+			?>
+			<a href="<?php esc_url($custom_module['link']); ?>" class="col">
+				<div class="postbox">
+					<div>
+						<div class="group">
+							<div class="dashicons-before <?php echo esc_attr($custom_module['icon']); ?>"></div>
+							<span><?php esc_html_e($custom_module['title'], 'osdxp-dashboard'); ?></span>
+							<p><?php esc_html_e($custom_module['subtitle'], 'osdxp-dashboard'); ?></p>
+						</div>
+					</div>
+				</div>
+			</a>
+			<?php
+		}
 		?>
 
 	</div>
--- templates/dashboard-actions-network-admin.php
+++ templates/dashboard-actions-network-admin.php
@@ -54,28 +54,6 @@
 				</div>
 			</a>
 		<?php endif; ?>
-
-        <?php
-        $custom_create_functionality_modules = [];
-        $custom_create_functionality_modules = apply_filters('osdxp_dashboard_create_functionality', $custom_create_functionality_modules);
-
-        foreach ($custom_create_functionality_modules as $custom_module) {
-            ?>
-            <a href="<?php esc_url($custom_module['link']); ?>" class="col">
-				<div class="postbox">
-					<div>
-						<div class="group">
-							<div class="dashicons-before <?php echo esc_attr($custom_module['icon']); ?>"></div>
-							<span><?php esc_html_e($custom_module['title'], 'osdxp-dashboard'); ?></span>
-							<p><?php esc_html_e($custom_module['subtitle'], 'osdxp-dashboard'); ?></p>
-						</div>
-						<div class="button button-primary"><?php esc_html_e($custom_module['button_text'], 'osdxp-dashboard'); ?></div>
-					</div>
-				</div>
-			</a>
-            <?php
-        }
-        ?>
 	</div>
 
 	<h2 class="title"><?php esc_html_e('Manage Functionality', 'osdxp-dashboard'); ?></h2>
@@ -123,24 +101,24 @@
 		<?php endif; ?>
 
 		<?php
-        $custom_manage_functionality_modules = [];
-        $custom_manage_functionality_modules = apply_filters('osdxp_dashboard_manage_functionality', $custom_manage_functionality_modules);
+		$custom_create_functionality_modules = [];
+		$custom_create_functionality_modules = apply_filters('osdxp_dashboard_create_functionality', $custom_create_functionality_modules);
 
-        foreach ($custom_manage_functionality_modules as $custom_module) {
-            ?>
-            <a href="<?php esc_url($custom_module['link']); ?>" class="col">
-                <div class="postbox">
-                    <div>
-                        <div class="group">
-                            <div class="dashicons-before <?php echo esc_attr($custom_module['icon']); ?>"></div>
-                            <span><?php esc_html_e($custom_module['title'], 'osdxp-dashboard'); ?></span>
-                            <p><?php esc_html_e($custom_module['subtitle'], 'osdxp-dashboard'); ?></p>
-                        </div>
-                    </div>
-                </div>
-            </a>
-            <?php
-        }
-        ?>
+		foreach ($custom_create_functionality_modules as $custom_module) {
+			?>
+			<a href="<?php esc_url($custom_module['link']); ?>" class="col">
+				<div class="postbox">
+					<div>
+						<div class="group">
+							<div class="dashicons-before <?php echo esc_attr($custom_module['icon']); ?>"></div>
+							<span><?php esc_html_e($custom_module['title'], 'osdxp-dashboard'); ?></span>
+							<p><?php esc_html_e($custom_module['subtitle'], 'osdxp-dashboard'); ?></p>
+						</div>
+					</div>
+				</div>
+			</a>
+			<?php
+		}
+		?>
 	</div>
  </div>

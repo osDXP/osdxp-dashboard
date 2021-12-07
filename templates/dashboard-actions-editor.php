0f674f7cb4e937e0a2fac3cc0b39440128c3c932
Al N <anitu@crowdfavorite.com>
2021-12-07 16:44:31 +0200

    add dashboard template for editors

--- templates/dashboard-actions-editor.php
+++ templates/dashboard-actions-editor.php
@@ -6,135 +6,76 @@
  * @package osdxp-dashboard
  */
 
+$campaign = get_permalink(get_page_by_path('event-promo', OBJECT, 'campaigns'));
+$campaign .= '?fl_builder';
 ?>
  <div id="dxp-actions">
-
-	<h2 class="title"><?php esc_html_e('Create Functionality', 'osdxp-dashboard'); ?></h2>
-	<div class="row large">
-		<?php if (current_user_can('publish_pages')) : ?>
-			<a href="<?php echo admin_url('post-new.php?post_type=page'); // phpcs:ignore?>" class="col">
-				<div class="postbox">
-					<div>
-						<div class="group">
-							<div class="dashicons-before dashicons-admin-page"></div>
-							<span><?php esc_html_e('Pages', 'osdxp-dashboard'); ?></span>
-							<p><?php esc_html_e('Create New Page', 'osdxp-dashboard'); ?></p>
-						</div>
-						<div class="button button-primary"><?php esc_html_e('New Page', 'osdxp-dashboard'); ?></div>
-					</div>
+	<h2 class="title"><?php esc_html_e('Quick Actions', 'osdxp-dashboard'); ?></h2>
+	<div class="row quick-actions">
+		<a href="<?php echo admin_url('post.php?post=2234&action=edit');  // phpcs:ignore?>" class="col">
+			<div class="quickbox">
+				<div class="group group-left">
+					<span><?php esc_html_e('Create new page', 'osdxp-dashboard'); ?></span>
+					<p><?php esc_html_e('Product Landing Page: eCommerce', 'osdxp-dashboard'); ?></p>
 				</div>
-			</a>
-		<?php endif; ?>
-
-		<?php if (current_user_can('publish_posts')) : ?>
-			<a href="<?php echo admin_url('post-new.php'); // phpcs:ignore?>" class="col">
-				<div class="postbox">
-					<div>
-						<div class="group">
-							<div class="dashicons-before dashicons-text-page"></div>
-							<span><?php esc_html_e('Articles', 'osdxp-dashboard'); ?></span>
-							<p><?php esc_html_e('Create New Article', 'osdxp-dashboard'); ?></p>
-						</div>
-						<div class="button button-primary"><?php esc_html_e('New Article', 'osdxp-dashboard'); ?></div>
-					</div>
+				<div class="group group-right">
+					<div class="dashicons-before dashicons-cart"></div>
 				</div>
-			</a>
-		<?php endif; ?>
-
-		<?php
-		$custom_create_functionality_modules = [];
-		$custom_create_functionality_modules = apply_filters(
-			'osdxp_dashboard_editor_create_functionality',
-			$custom_create_functionality_modules
-		);
-
-		foreach ($custom_create_functionality_modules as $custom_module) {
-			?>
-			<a href="<?php echo esc_url($custom_module['link']); ?>" class="col">
-				<div class="postbox">
-					<div>
-						<div class="group">
-							<div class="dashicons-before <?php echo esc_attr($custom_module['icon']); ?>"></div>
-							<span><?php echo esc_html($custom_module['title']); ?></span>
-							<p><?php echo esc_html($custom_module['subtitle']); ?></p>
-						</div>
-						<div class="button button-primary">
-							<?php echo esc_html($custom_module['button_text']); ?>
-						</div>
-					</div>
+			</div>
+		</a>
+		<a href="#" class="col">
+			<div class="quickbox">
+				<div class="group group-left">
+					<span><?php esc_html_e('Create new event promotion', 'osdxp-dashboard'); ?></span>
+					<p><?php esc_html_e('Event Promotion Landing Page', 'osdxp-dashboard'); ?></p>
 				</div>
-			</a>
-			<?php
-		}
-		?>
+				<div class="group group-right">
+					<div class="dashicons-before dashicons-calendar"></div>
+				</div>
+			</div>
+		</a>
+		<a href="#" class="col">
+			<div class="quickbox">
+				<div class="group group-left">
+					<span><?php esc_html_e('Create new webinar', 'osdxp-dashboard'); ?></span>
+					<p><?php esc_html_e('Webinar: Including Sign-ups', 'osdxp-dashboard'); ?></p>
+				</div>
+				<div class="group group-right">
+					<div class="dashicons-before dashicons-tickets-alt"></div>
+				</div>
+			</div>
+		</a>
+		<a href="<?php echo esc_url($campaign); ?>" class="col">
+			<div class="quickbox">
+				<div class="group group-left">
+					<span><?php esc_html_e('Create new landing page', 'osdxp-dashboard'); ?></span>
+					<p><?php esc_html_e('Campaign Landing Page', 'osdxp-dashboard'); ?></p>
+				</div>
+				<div class="group group-right">
+					<div class="dashicons-before dashicons-text-page"></div>
+				</div>
+			</div>
+		</a>
 	</div>
 
 	<h2 class="title"><?php esc_html_e('Manage Functionality', 'osdxp-dashboard'); ?></h2>
-	<div class="row">
-		<?php if (current_user_can('edit_pages')) : ?>
-			<a href="<?php echo admin_url('edit.php?post_type=page');  // phpcs:ignore?>" class="col">
-				<div class="postbox">
-					<div>
-						<div class="group">
-							<div class="dashicons-before dashicons-admin-page"></div>
-							<span><?php esc_html_e('Pages', 'osdxp-dashboard'); ?></span>
-							<p><?php esc_html_e('Manage Pages', 'osdxp-dashboard'); ?></p>
-						</div>
-					</div>
+	<div class="row large">
+		<a href="#cf-assistant-popup" class="col">
+			<div class="postbox">
+				<div class="group">
+					<span><?php esc_html_e('AI Site Assistant', 'osdxp-dashboard'); ?></span>
+					<p><?php esc_html_e('I\'m here to help!', 'osdxp-dashboard'); ?></p>
 				</div>
-			</a>
-		<?php endif; ?>
-
-		<?php if (current_user_can('edit_posts')) : ?>
-			<a href="<?php echo admin_url('edit.php');  // phpcs:ignore?>" class="col">
-				<div class="postbox">
-					<div>
-						<div class="group">
-							<div class="dashicons-before dashicons-text-page"></div>
-							<span><?php esc_html_e('Articles', 'osdxp-dashboard'); ?></span>
-							<p><?php esc_html_e('Manage Articles', 'osdxp-dashboard'); ?></p>
-						</div>
-					</div>
+				<div class="dashicons-before dashicons-businesswoman"></div>
+			</div>
+		</a>
+		<a href="<?php echo admin_url('admin.php?page=ab-testing');  // phpcs:ignore?>" class="col">
+			<div class="postbox">
+				<div class="group">
+					<span><?php esc_html_e('A/B Testing', 'osdxp-dashboard'); ?></span>
+					<p><?php esc_html_e('Manage A/B Campaigns', 'osdxp-dashboard'); ?></p>
 				</div>
-			</a>
-		<?php endif; ?>
-
-		<?php if (current_user_can('upload_files')) : ?>
-			<a href="<?php echo admin_url('upload.php');  // phpcs:ignore?>" class="col">
-				<div class="postbox">
-					<div>
-						<div class="group">
-							<div class="dashicons-before dashicons-admin-media"></div>
-							<span><?php esc_html_e('Media', 'osdxp-dashboard'); ?></span>
-							<p><?php esc_html_e('Manage Media', 'osdxp-dashboard'); ?></p>
-						</div>
-					</div>
-				</div>
-			</a>
-		<?php endif; ?>
-
-		<?php
-		$custom_manage_functionality_modules = [];
-		$custom_manage_functionality_modules = apply_filters(
-			'osdxp_dashboard_editor_manage_functionality',
-			$custom_manage_functionality_modules
-		);
-
-		foreach ($custom_manage_functionality_modules as $custom_module) {
-			?>
-			<a href="<?php echo esc_url($custom_module['link']); ?>" class="col">
-				<div class="postbox">
-					<div>
-						<div class="group">
-							<div class="dashicons-before <?php echo esc_attr($custom_module['icon']); ?>"></div>
-							<span><?php echo esc_html($custom_module['title']); ?></span>
-							<p><?php echo esc_html($custom_module['subtitle']); ?></p>
-						</div>
-					</div>
-				</div>
-			</a>
-			<?php
-		}
-		?>
+				<div class="dashicons-before dashicons-chart-area"></div>
+			</div>
+		</a>
 	</div>
- </div>
